b423dcdcedd64d8af0e490cdf62635485f3beb21
AhmedMaaroufi <ahmed.maaroufi@cirvianum.cat>
2024-12-12 10:09:18 +0100

    Cotxes modificats

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Model S", "Tesla", 2022, 250.0);
+        Vehicle vehicle1 = new Vehicle("GT-R", "Nissan", 2019, 270.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Mustang", "Ford", 1965, 220.0);
+        Vehicle vehicle2 = new Vehicle("Almera", "Nissan", 2006, 170.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
