4970f5bdcf1d55d189bd5d5f2b589cf22dcc03f4
AhmedMaaroufi <ahmed.maaroufi@cirvianum.cat>
2024-12-18 11:01:18 +0100

    2n  Canvi de marca i models dels coches

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("GT-R", "Nissan", 2019, 270.0);
+        Vehicle vehicle1 = new Vehicle("c3", "Citroen", 2019, 160.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Almera", "Nissan", 2006, 170.0);
+        Vehicle vehicle2 = new Vehicle("c2", "Citroen", 2020, 135.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
